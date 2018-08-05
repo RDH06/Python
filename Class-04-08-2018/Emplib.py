4232387979166dd8a1d19c3cffefb24358770dd6
RDH06 <41722232+RDH06@users.noreply.github.com>
2018-08-05 17:59:38 +0530

    Create Emplib.py

---
+++ Class-04-08-2018/Emplib.py
@@ -0,0 +1,7 @@
+import os
+class Emp:
+        def __init__(self,Name,Dept):
+            self.Name = Name
+            self.Dept = Dept
+        def show (self):
+            print (self.Name, self.Dept)
